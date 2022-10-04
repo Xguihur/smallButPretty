66530ad3b287d23769129a16cda2f7aa95615868
Basal <8813036+xgh329179227@user.noreply.gitee.com>
2022-10-04 12:15:42 +0800

    feat: 添加了路由组件，设置了导航守卫以及完成了页面的守卫，以及一些token验证登录

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 import Vue from "vue";
 import App from "./App";
 
+// 导入路由
+import router from "./router/index.js"
+
 // 导入uview
 import uView from '@/uni_modules/uview-ui'
 Vue.use(uView)
@@ -9,20 +12,24 @@ Vue.use(uView)
 Vue.config.productionTip = false;
 
 App.mpType = "app";
+App.router = router;
 
 const app = new Vue({
-  ...App,
+	...App,
 });
+
 app.$mount();
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from "vue";
+import {
+	createSSRApp
+} from "vue";
 import App from "./App.vue";
 export function createApp() {
-  const app = createSSRApp(App);
-  return {
-    app,
-  };
+	const app = createSSRApp(App);
+	return {
+		app,
+	};
 }
 // #endif
